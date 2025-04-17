03367038a57b20e6aed14413529a049160a3e678
ohdoglas <dughlaisdd@gmail.com>
2025-04-17 16:18:18 -0300

    feat: qr code gen service created

---
+++ backend/src/service/QRCode.ts
@@ -0,0 +1,39 @@
+import QRCode  from "qrcode";
+import fs from "fs";
+import path from "path";
+
+var options: QRCode.QRCodeToDataURLOptionsOther = {
+    errorCorrectionLevel: 'H',
+    type: 'image/png',
+    margin: 1,
+    color: {
+        dark:"#000000",
+        light: "#FFFFFF"
+    }
+};
+
+export default class QRCodeService {
+    async generateQRCode(url: string) {
+        try {
+            if (typeof url !== "string" || !url.startsWith("http")) {
+                throw new Error("Invalid URL");
+            };
+            console.log("Generating QR code for URL (service): ", url);
+
+            const QRResponse = await QRCode.toDataURL(url, options);
+
+            const filePath = path.join(__dirname, "../public/generatedQRCode/QRCode.png");
+
+            const base64Data = QRResponse.replace(/^data:image\/png;base64,/, "");
+
+            fs.writeFileSync(filePath, base64Data, 'base64');
+
+            console.log("QRCode saved on: ", filePath);
+
+            return QRResponse;
+        } catch (error) {
+            console.error(`Error generating QRCode: ${error}`);
+            throw error;
+        }
+    }
+}
