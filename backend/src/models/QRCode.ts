d7551ca8b92e1844a3d6abd44168ac4fa7259c54
ohdoglas <dughlaisdd@gmail.com>
2025-04-18 11:19:43 -0300

    feat: minor changes on qrcode prisma model(base64 to text), QRCode class implements

--- backend/src/models/QRCode.ts
+++ backend/src/models/QRCode.ts
@@ -1,3 +1,4 @@
+import prisma from "../config/prisma";
 import TQRCode from "../types/TQRCode";
 import { v4 as uuidv4 } from "uuid";
 
@@ -20,4 +21,59 @@ export default class QRCode {
         this.private = props.private ?? false;
         this.scanCount = props.scanCount ?? 0;
     }
+
+    static async isPrivate(id: string) {
+
+        try {
+            const state = await prisma.qRCode.findUnique({
+                where:{ id: id},
+                select: { private: true }
+            });
+
+            return state?.private;
+        } catch (error) {
+            console.error(error);
+            throw new Error(`Error checking if data is private: ${error}`, )
+        }
+    }
+
+    static async incrementScanCount(id: string) {
+        try {
+            const currentCount = await prisma.qRCode.findUnique({
+                where: { id: id},
+                select: { scanCount: true }
+            });
+
+            if (currentCount === null) {
+                return false;
+            }
+
+            const newCount = currentCount!.scanCount + 1;
+
+            const updateCount = await prisma.qRCode.update({
+                where: { id: id},
+                data: { scanCount: newCount }
+            });
+
+            return updateCount;
+        } catch (error) {
+            console.error(error);
+            throw new Error(`Error trying to increment Scan Count: ${error}`);
+        }
+    }
+
+    static async updateField(id: string, data: Partial<TQRCode>) {
+        try {
+            const updateQRCode = await prisma.qRCode.update({
+                where: { id: id },
+                data,
+            });
+
+            return updateQRCode;
+        } catch (error) {
+            console.error(error);
+            throw new Error(`Error while field update: ${error}`);
+        }
+    }
+
 }
