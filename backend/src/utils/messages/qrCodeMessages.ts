3396c4af997a7533a32d16eb8d3dff7daff3b210
ohdoglas <dughlaisdd@gmail.com>
2025-04-19 11:13:58 -0300

    update: new messages (server, qrcodes and supabase)

---
+++ backend/src/utils/messages/qrCodeMessages.ts
@@ -0,0 +1,13 @@
+const QRCODEMSG = {
+    ERR: {
+        MISSING_QRCODE_IMG_FILE: "Please, insert an image for upload.",
+        MISSING_QRCODE_ID: "ID is missing. Please provide an ID to complete the request.",
+        WRONG_UUID_FORMAT: "Invalid ID. The provided ID is not a valid uuid.",
+
+    },
+    SUCCESS: {
+
+    }
+}
+
+export default QRCODEMSG;
---
+++ backend/src/utils/messages/supaMessages.ts
@@ -0,0 +1,10 @@
+const SUPAMSG = {
+    ERR: {
+        IMG_UPLOAD_FAILED_SERVER: 'Unable to upload the image due to a server error. Please try again later. If the problem persists, please contact our support system.',
+    },
+    SUCCESS: {
+        IMG_UPLOADED_SUCCESSFULLY: 'Product image has been successfully updated. Here is the new image URL.',
+    }
+}
+
+export default SUPAMSG;
