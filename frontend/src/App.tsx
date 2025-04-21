f2bf7af931ef36a5a0a086099a9375d5b92ee01c
ohdoglas <dughlaisdd@gmail.com>
2025-04-21 18:32:18 -0300

    feat: 404 page created - structure and style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/public/Home";
 import Generator from "./pages/public/Generator";
+import NotFound from "./pages/public/NotFound";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
           <Route>
             <Route path="/" element={<Home />} />
             <Route path="/generator" element={<Generator />} />
+            <Route path="/404-not-found" element={<NotFound />}/>
           </Route>
         </Routes>
     </Router>
---
+++ frontend/src/pages/public/NotFound.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "../components/header";
+import "../../css/styles/public/NotFound.css"
+import { Link } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+    return (
+        <div className="page-three">
+            <Header />
+            <div className="main_content">
+                <div className="main_content_title">
+                    <h1 className="main_content_title_h1">
+                    404 - Page Not Found
+                    </h1>
+                </div>
+                <div className="main_content_subtitle">
+                    <h1 className="main_content_subtitle_h3">
+                    Oops! The page you’re looking for doesn’t exist.
+                    It might have been moved, deleted, or never existed.
+                    </h1>
+                </div>
+                <Link to={"/generator"}>  <button className="main_content_button">Back to QR.App</button></Link>
+                <div className="main_content_404_img">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound;
