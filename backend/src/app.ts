02a2b501d9d45f99c76b9c8c7d921d5a56346f0a
ohdoglas <dughlaisdd@gmail.com>
2025-04-22 08:14:37 -0300

    feat: cors added to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express, { Application } from "express";
 import "dotenv/config";
+import cors from "cors";
 import routes from "./routes/router";
 
 const app: Application = express();
+
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,5 @@ import app from './app';
 const PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`QR.APP running on PORT: ${PORT}`)
+    console.log(`QR.APP running on PORT: ${PORT}`);
 });
