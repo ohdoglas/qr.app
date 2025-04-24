64fc39168576c3a51faf232a60aef6034e88d78b
ohdoglas <dughlaisdd@gmail.com>
2025-04-24 13:53:04 -0300

    feat: qrcode history middleware, controller and route created

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 import QRCodeGenMiddleware from "../middlewares/authenticateURL";
 import QRCodeGen from "../controllers/QRCodeGen";
+import QRCodeHistoryMiddleware from "../middlewares/authenticateQRCodeHistory";
+import QRCodeHistory from "../controllers/QRCodeHistory";
+
 
 const QRCodeServiceGenerator = {
     middleware: new QRCodeGenMiddleware().authenticate,
     controller: new QRCodeGen().controller
 };
 
+const QRCodeHistoryService = {
+    middleware: new QRCodeHistoryMiddleware().authenticate,
+    controller: new QRCodeHistory().controller
+}
+
 const routes = Router();
 
 routes.post('/qrcode-generator', QRCodeServiceGenerator.middleware, QRCodeServiceGenerator.controller);
+routes.get('/history', QRCodeHistoryService.middleware, QRCodeHistoryService.controller);
+
 
 export default routes;
