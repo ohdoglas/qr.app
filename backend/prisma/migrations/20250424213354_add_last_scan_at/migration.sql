68aa7b85361ffd862e40a113045d7674ae9b750b
ohdoglas <dughlaisdd@gmail.com>
2025-04-24 21:41:07 -0300

    changes: add prisma migrations removed

---
+++ backend/prisma/migrations/20250424213354_add_last_scan_at/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "QRCode" ADD COLUMN     "lastScanAt" TIMESTAMP;
---
+++ backend/prisma/migrations/20250424213634_remove_last_scan_at/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastScanAt` on the `QRCode` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "QRCode" DROP COLUMN "lastScanAt";
